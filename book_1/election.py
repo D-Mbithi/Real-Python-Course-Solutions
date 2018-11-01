127c3d1efbf0ece6ea0c2b46a3446b1d66fe0ee7
d-mbithi <denmbithi@gmail.com>
2018-11-01 16:12:48 +0300

    add solution to election problem

--- book_1/election.py
+++ book_1/election.py
@@ -2,34 +2,39 @@ from random import random
 
 trials = 10000
 
-total_A_wins = 0
-total_B_wins = 0
+canditate_A_wins = 0
+canditate_B_wins = 0
 
-
-for trial in range(0, trials):
+# loop throught the set number of trials
+for trial in range(0,trials):
     A_win = 0
     B_win = 0
 
-    if random() < .87:
+    # check winner of region 1
+    if random() < 0.87:
         A_win += 1
     else:
         B_win += 1
 
-    if random() < .65:
+    # check winner of region 2
+    if random() < 0.65:
         A_win += 1
     else:
         B_win += 1
 
-    if random() > .17:
+    # check winner of region 3
+    if random() < 0.17:
         A_win += 1
     else:
         B_win += 1
 
 
-    if A_win > B_win:
-        total_B_wins += 1
+    # check the overall winner
+    if A_win < B_win:
+        canditate_B_wins += 1
     else:
-        total_B_wins += 1
+        canditate_A_wins += 1
 
-print(f"Total A win is: {total_A_wins}/10000")
-print(f"Total B win is: {total_B_wins}/10000")
+print("Canditate A average wins: {}/10000".format( canditate_A_wins))
+print("Canditate B average wins: {}/10000".format(canditate_B_wins))
+
