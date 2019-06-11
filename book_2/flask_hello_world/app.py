19173ec0dc84f27d79a3362fc0a3d97f2ce0df27
Dennis Mutuku <denmbithi@gmail.com>
2019-06-11 03:50:36 +0300

    Add hello flask app

--- book_2/flask_hello_world/app.py
+++ book_2/flask_hello_world/app.py
@@ -6,7 +6,37 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/hello')
 def index():
+    """Index routing."""
     return "Hello world"
+
+
+@app.route('/test/<search_query>')
+def search(search_query):
+    """Search routing."""
+    return search_query
+
+
+@app.route('/integer/<int:value>')
+def int_type(value):
+    """Integer value routing."""
+    print(value + 1)
+    return "correct"
+
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    """Path value routing."""
+    print(value)
+    return "correct"
+
+
+@app.route("/name/<name>")
+def home(name):
+    """Home routing view."""
+    if name.lower() == 'dennis':
+        return "Hello, {}".format(name), 200
+    else:
+        return "Not found", 404
 
 
 if __name__ == "__main__":
